d26285c8c9f63c302ef1c8acf9e30ad9f3af534c
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-08-01 22:47:28 +0300

    Create action

--- src/action.ts
+++ src/action.ts
@@ -1,3 +1,41 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
 import * as utility from './utility'
+
+export async function createReadme(packagePath: string, templatePath: string, config: any): Promise<string> {
+  const packageData = await utility.readData(packagePath, 'json')
+  const template = await utility.read(templatePath)
+  const values = {
+    package: packageData,
+    dependenciesFormatted: formatDependencies(packageData, config)
+  }
+
+  const format = utility.formatValues(template, values)
+
+  return format
+}
+
+export function formatDependencies(packageData: any, config: any) {
+  let format = ''
+
+  if (packageData.dependencies != null) {
+    const keys = Object.keys(packageData.dependencies)
+
+    if (keys.length > 0) {
+      for (const key of keys) {
+        const value = packageData.dependencies[key]
+        const values = {
+          package: packageData,
+          dependency: {
+            name: key,
+            version: value
+          }
+        }
+
+        format += utility.formatValues(config.dependency, values)
+      }
+    } else {
+      format += config.dependenciesEmpty
+    }
+  }
+
+  return format
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
 import * as utility from './utility'
+import * as action from './action'
 
 run()
 
 async function run(): Promise<void> {
   try {
-    console.log('This is a draft action.')
+    const packagePath = core.getInput('package', {required: true})
+    const template = core.getInput('template', {required: true})
+    const config = await utility.readConfig()
+    const result = await action.createReadme(packagePath, template, config)
+
+    await utility.setOutput(result)
   } catch (error) {
     core.setFailed(error.message)
   }
